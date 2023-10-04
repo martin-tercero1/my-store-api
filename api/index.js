9e838f67faaf15791df731c534d42c7908df8fb0
martin-tercero1 <martintercero20@hotmail.com>
2023-10-04 09:55:01 -0600

    Moved the cors middleware after the routing

--- api/index.js
+++ api/index.js
@@ -12,23 +12,6 @@ const port = process.env.PORT || 8016;
 
 app.use(express.json());
 
-const whitelist = [
-  'http://localhost:8080',
-  'https://martin-tercero.com',
-  'https://my-store-api-git-main-martin-terceros-projects.vercel.app',
-  'https://my-store-api-martin-terceros-projects.vercel.app',
-  'https://my-store-api-seven.vercel.app',
-];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed'));
-    }
-  },
-};
-app.use(cors(options));
 
 app.get('/api', (req, res) => {
   res.send('Server');
@@ -40,6 +23,24 @@ app.get('/api/home', (req, res) => {
 
 routerApi(app);
 
+const whitelist = [
+  'http://localhost:8016',
+  'https://martin-tercero.com',
+  'https://my-store-api-git-main-martin-terceros-projects.vercel.app',
+  'https://my-store-api-martin-terceros-projects.vercel.app',
+  'https://my-store-api-seven.vercel.app',
+];
+const options = {
+  origin: (origin, callback) => {
+    console.log(origin)
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed'));
+    }
+  },
+};
+app.use(cors(options));
 // Error middlewares should be defined after the routing
 // Also the order of execution is the order of declaration
 app.use(errorLogger);
