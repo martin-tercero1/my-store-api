7de0539e034906d9ceb227507bc71dd8f20a7cb2
martin-tercero1 <martintercero20@hotmail.com>
2024-05-28 20:23:36 -0600

    Fix orders migrations

--- db/migrations/20240505204523-create-user.js
+++ db/migrations/20240505204523-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { USERS_TABLE } = require('./../models/usersModel');
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
--- db/migrations/20240528152735-order.js
+++ db/migrations/20240528152735-order.js
@@ -1,14 +1,40 @@
 'use strict';
 
 const {
-  OrdersSchema,
   ORDERS_TABLE,
 } = require('./../models/ordersModel');
+const { CUSTOMERS_TABLE } = require('./../models/customersModel');
+
+const { DataTypes, Sequelize } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDERS_TABLE, OrdersSchema);
+    await queryInterface.createTable(ORDERS_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMERS_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      }
+    });
   },
 
   async down(queryInterface, Sequelize) {
